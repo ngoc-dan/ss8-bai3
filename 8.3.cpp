63a60d6975e98fa98106efbafc26e412c9edca26
ngoc-dan <ngocdan191026@gmail.com>
2024-11-28 22:40:27 +0700

    Add files via upload

--- 8.3.cpp
+++ 8.3.cpp
@@ -3,11 +3,12 @@ int main(){
 	int n;
 	printf("nhap hang va cot : ");
 	scanf("%d",&n);
-	int arr[n][n]={{nn},{n}};
+	int arr[n][n]={};
 	
 	for(int i=0;i<n;i++){
 		printf("\n");
 		for(int t=0;t<n;t++){
+			arr[i][t]=n;
 			printf("%d ",arr[i][t]);
 		}
 	}
